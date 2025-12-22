b33d7d0469b7d5c55fc936a05ca347f9e61b5524
unknown <theasifzone@gmail.com>
2025-12-22 17:07:13 +0500

    deploy code

--- backend/server.js
+++ backend/server.js
@@ -3,23 +3,26 @@ dotenv.config();
 import express from "express";
 import connectDB from "./db/db.js";
 import fileUpload from "./routes/fileUpload.route.js";
-import userRoute from "./routes/user.route.js"
+import userRoute from "./routes/user.route.js";
 import cors from "cors";
+
 const app = express();
 
+// CORS
 app.use(cors({
-    origin:"http://localhost:5173",
-    methods: ["GET","POST","PUT","DELETE"],
-    credentials:true
+  origin: "https://your-vercel-frontend-url.vercel.app", // localhost hatao
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
 }));
-app.use(express.json()); // to accept json data
 
+app.use(express.json());
+
+// Routes
 app.use("/api", fileUpload);
-app.use("/api",userRoute)
+app.use("/api", userRoute);
 
-connectDB().then(() => {
-    app.listen(process.env.PORT, () => {
-        console.log(`Server running on port ${process.env.PORT}`);
-    });
-})
-.catch((error) => console.log(error));
+// DB connect
+connectDB().catch((err) => console.log(err));
+
+//  MOST IMPORTANT
+export default app;
