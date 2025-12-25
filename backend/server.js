b49db31de9503ad889969080193f5dfc7801c7b9
unknown <theasifzone@gmail.com>
2025-12-25 12:54:11 +0500

    updated code

--- backend/server.js
+++ backend/server.js
@@ -11,8 +11,7 @@ const PORT = process.env.PORT
 
 // CORS
 app.use(cors());
-app.use(express.json({limit:"50mb"}));
-app.use(express.urlencoded({extended:true}))
+app.use(express.json());
 // DB connect
 await connectDB().catch((err) => console.log(err));
 // Routes
