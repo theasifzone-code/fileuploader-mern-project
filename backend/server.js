623a35da148ff94906e185614977243057d3ee32
unknown <theasifzone@gmail.com>
2025-12-25 12:49:00 +0500

    update backend server and add vercel config

--- backend/server.js
+++ backend/server.js
@@ -6,13 +6,13 @@ import fileUpload from "./routes/fileUpload.route.js";
 import userRoute from "./routes/user.route.js";
 import cors from "cors";
 
-
 const app = express();
 const PORT = process.env.PORT
 
 // CORS
 app.use(cors());
-app.use(express.json());
+app.use(express.json({limit:"50mb"}));
+app.use(express.urlencoded({extended:true}))
 // DB connect
 await connectDB().catch((err) => console.log(err));
 // Routes
